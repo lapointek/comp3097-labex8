6935f55f4fb7583813b494b2cd0e1bbbdfe8fa71
lapointek <156107790+lapointek@users.noreply.github.com>
2025-03-11 14:43:09 -0400

    initial commit

--- labex8/ViewController.swift
+++ labex8/ViewController.swift
@@ -1,17 +1,32 @@
-//
-//  ViewController.swift
-//  labex8
-//
-//  Created by Kevin Lapointe on 2025-03-11.
-//
+
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    // 1st step - access to app delegate
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 2nd step - access to context
+        let context = appDelegate.persistentContainer.viewContext
+        
+        // 3rd step - creating some data and prepare to write into it
+        // access to an instance of our table entity
+        let newContact = NSEntityDescription.insertNewObject(forEntityName: "Contact", into: context)
+        
+        newContact.setValue("test first name", forKey: "firstName")
+        newContact.setValue("test last name", forKey: "lastName")
+        newContact.setValue("test email", forKey: "email")
+        newContact.setValue(20, forKey: "age")
+        
+        // save it to our table
+        appDelegate.saveContext()
+        
     }
 
 
