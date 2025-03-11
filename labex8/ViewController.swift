72bd74d494f147cf02caada89106bf48367c8373
lapointek <156107790+lapointek@users.noreply.github.com>
2025-03-11 15:54:18 -0400

    added do while loop in ViewController

--- labex8/ViewController.swift
+++ labex8/ViewController.swift
@@ -17,16 +17,42 @@ class ViewController: UIViewController {
         
         // 3rd step - creating some data and prepare to write into it
         // access to an instance of our table entity
-        let newContact = NSEntityDescription.insertNewObject(forEntityName: "Contact", into: context)
-        
-        newContact.setValue("test first name", forKey: "firstName")
-        newContact.setValue("test last name", forKey: "lastName")
-        newContact.setValue("test email", forKey: "email")
-        newContact.setValue(20, forKey: "age")
+//        let newContact = NSEntityDescription.insertNewObject(forEntityName: "Contact", into: context)
+//        
+//        newContact.setValue("test first name", forKey: "firstName")
+//        newContact.setValue("test last name", forKey: "lastName")
+//        newContact.setValue("test email", forKey: "email")
+//        newContact.setValue(20, forKey: "age")
         
         // save it to our table
-        appDelegate.saveContext()
+//      appDelegate.saveContext()
         
+        fetchData(context)
+    }
+    
+    func fetchData(_ context: NSManagedObjectContext ){
+        // retrieval process
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Contact")
+        
+        // where condition => predicate
+        do{
+           let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let firstName = result.value(forKey: "firstName"){
+                        let firstName = firstName as! String
+                        print(firstName)
+                    }
+                    if let firstName = result.value(forKey: "firstName"){
+                        let firstName = firstName as! String
+                        print(firstName)
+                    }
+                }
+            }
+            
+        }catch{
+            print(error)
+        }
     }
 
 
